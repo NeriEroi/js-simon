907b1cdd060d98ee63fc42aae278e61760df1c37
Michele <micheleierniero@gmail.com>
2021-07-29 15:29:20 +0200

    timer e numeri da inserire con esito

--- js/main.js
+++ js/main.js
@@ -12,8 +12,35 @@ for (var i = 0; i < 5; i++) {
 
 alert("Guarda attentamente questi cinque numeri: " + numeriCPU);
 
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+var numeriUtente = [];
 
 
+setTimeout(chiediNumeriUtente, 1000);
+function chiediNumeriUtente()
+{
+  for (var i = 0; i < 5; i++)
+  {
+    var numeroInserito = parseInt(prompt('Inserisci un numero che ricordi:'));
+    if (numeriCPU.includes(numeroInserito)) {
+      numeriUtente.push(numeroInserito);
+    }
+    console.log(numeroInserito);
+  }
+  if (numeriUtente < 1)
+  {
+    alert('Peccato, non hai ricordato nessun numero.');
+    console.log('nessun numero ricordato.');
+  }
+  else 
+  {
+    console.log('numeri inseriti correttamente: ' + numeriUtente);
+    alert('Complimenti!\nHai ricordato ' + (numeriUtente.length) + ' numeri!\nQuesti sono i numeri che hai ricordato: ' + numeriUtente)
+  }
+}
 
 
 
